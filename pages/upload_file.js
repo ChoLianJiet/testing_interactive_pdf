324f66b191f07a174c60c6ac720dd4c71e2585a8
ChoLianJiet <lianjiet_choo95@hotmail.com>
2022-01-01 23:36:05 +0800

    commit

--- pages/upload_file.js
+++ pages/upload_file.js
@@ -1,6 +1,7 @@
 import UploadImageWithData from "../components/storage/UploadImageWithData";
 import firebase from 'firebase/compat/app'
 import {withProtected} from '../firebase/routes'
+import UploadFile from '../components/storage/UploadFile'
 
 const UploadFile = ({auth}) => {
 
